79dd9f41623a511ba888a8ba348b3f4145b9ef75
toyamarodrigo <toyama.rodrigo@gmail.com>
2019-12-20 03:09:54 -0300

    added Invoice Controller

---
+++ src/main/java/com/rt/springboot/app/controllers/InvoiceController.java
@@ -0,0 +1,47 @@
+package com.rt.springboot.app.controllers;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.rt.springboot.app.models.entity.Client;
+import com.rt.springboot.app.models.entity.Invoice;
+import com.rt.springboot.app.models.service.IClientService;
+
+@Controller
+@RequestMapping("/factura")
+@SessionAttributes("invoice")
+public class InvoiceController {
+	
+	@Autowired
+	private IClientService clientService;
+	
+	// /factura/form/{client.id}
+	@GetMapping("/form/{client.id}")
+	public String create(@PathVariable(value = "clientId") Long clientId, Map<String, Object> model, RedirectAttributes flash) {
+		
+		Client client = clientService.findOne(clientId);
+		if(client == null) {
+			flash.addAttribute("error", "El cliente no existe en la base de datos");
+			return "redirect:/list";
+		}
+		
+		Invoice invoice = new Invoice();
+		invoice.setClient(client);
+		
+		model.put("invoice", invoice);
+		model.put("title", "Create invoice");
+		
+		
+		return "factura/form";
+	}
+	
+	
+	
+}

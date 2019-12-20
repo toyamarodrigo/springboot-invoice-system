027c92a459318d17e6055fb58cff20f3745eb868
toyamarodrigo <toyama.rodrigo@gmail.com>
2019-12-20 01:56:56 -0300

    added Invoice entity

--- src/main/java/com/rt/springboot/app/models/entity/Client.java
+++ src/main/java/com/rt/springboot/app/models/entity/Client.java
@@ -1,13 +1,18 @@
 package com.rt.springboot.app.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -20,46 +25,101 @@ import org.springframework.format.annotation.DateTimeFormat;
 @Entity
 @Table(name = "clients")
 public class Client implements Serializable {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotEmpty
 	private String firstName;
-	
+
 	@NotEmpty
 	private String lastName;
-	
+
 	@NotEmpty
 	@Email
 	private String email;
-	
+
 	@NotNull
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date createAt;
-	
-	private String photo;
-	
-	private static final long serialVersionUID = 1L;
-	
-	public static long getSerialVersionuid() { return serialVersionUID; }
 
+	@OneToMany(mappedBy = "client", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Invoice> invoices;
+
+	public Client() {
+		invoices = new ArrayList<Invoice>();
+	}
+
+	private String photo;
+
+	private static final long serialVersionUID = 1L;
+
+	public static long getSerialVersionuid() {
+		return serialVersionUID;
+	}
 
 	/*----- Getters & Setters -----*/
-	public Long getId() { return id; }
-	public void setId(Long id) { this.id = id; }
-	public String getFirstName() { return firstName; }
-	public void setFirstName(String firstName) { this.firstName = firstName; }
-	public String getLastName() { return lastName; }
-	public void setLastName(String lastName) { this.lastName = lastName; }
-	public String getEmail() { return email; }
-	public void setEmail(String email) { this.email = email; }
-	public Date getCreateAt() { return createAt; }
-	public void setCreateAt(Date createAt) { this.createAt = createAt; }
-	public String getPhoto() { return photo; }
-	public void setPhoto(String photo) { this.photo = photo; }
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public String getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+
+	public List<Invoice> getInvoices() {
+		return invoices;
+	}
+
+	public void setInvoices(List<Invoice> invoices) {
+		this.invoices = invoices;
+	}
+	
+	public void addInvoice(Invoice invoice) {
+		invoices.add(invoice);
+	}
 
 }
---
+++ src/main/java/com/rt/springboot/app/models/entity/Invoice.java
@@ -0,0 +1,84 @@
+package com.rt.springboot.app.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "facturas")
+public class Invoice implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String description;
+	private String observation;
+
+	@Temporal(TemporalType.DATE)
+	@Column(name = "create_at")
+	private Date createAt;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Client client;
+	
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
+	}
+	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getObservation() {
+		return observation;
+	}
+
+	public void setObservation(String observation) {
+		this.observation = observation;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	public Client getCliente() {
+		return client;
+	}
+
+	public void setCliente(Client cliente) {
+		this.client = cliente;
+	}
+
+}

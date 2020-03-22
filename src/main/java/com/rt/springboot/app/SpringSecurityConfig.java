e2f1ba922b50f70f21b4871efc7d53f9934d1173
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-03-21 22:46:31 -0300

    agregado clase SpringSecurityConfig, metodo de registro en memoria de ejemplo

---
+++ src/main/java/com/rt/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.rt.springboot.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	// Metodo de registro
+	// en memoria x ahora
+	@Autowired // para poder inyectar
+	public void configuredGlobal(AuthenticationManagerBuilder builder) throws Exception {
+		
+		PasswordEncoder encoder = passwordEncoder();
+		
+		// UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		// Manera 1
+		// UserBuilder users = User.builder().passwordEncoder(password -> encoder.encode(password));
+		
+		// Manera 2
+		// UserBuilder users = User.builder().passwordEncoder(encoder::encode);
+		
+		// Manera 3
+		UserBuilder users = User.builder().passwordEncoder(password -> {
+			return encoder.encode(password);
+		});
+		
+		builder.inMemoryAuthentication()
+		.withUser(users.username("admin").password("12345").roles("ADMIN","USER"))
+		.withUser(users.username("rodri").password("12345").roles("USER"));
+		
+		
+		
+	}
+	
+}

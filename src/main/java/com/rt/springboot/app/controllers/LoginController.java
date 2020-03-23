fed464a65d09b8cb0813537df639dfabf29ecfcf
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-03-23 20:49:17 -0300

    agregado aviso de login incorrecto

--- src/main/java/com/rt/springboot/app/controllers/LoginController.java
+++ src/main/java/com/rt/springboot/app/controllers/LoginController.java
@@ -5,17 +5,22 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 
 	@GetMapping("/login")
-	public String login(Model model, Principal principal, RedirectAttributes flash) {
+	public String login(@RequestParam(value = "error", required = false) String error,  Model model, Principal principal, RedirectAttributes flash) {
 		
 		if(principal != null) {
 			flash.addAttribute("info", "Ya ha iniciado sesión anteriormente");
 			return "redirect:/";
+		}
+		
+		if(error != null) {
+			model.addAttribute("error", "Error en el login: Nombre de usuario o contraseña incorrecta, por favor vuelva a intentarlo.");
 		}
 		
 		return "login";

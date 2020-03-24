295f308b90f5388016bb4976bdb48f3485180199
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-03-23 21:31:04 -0300

    agregado sign in y log out con alertas. dropdown con bootstrap

--- src/main/java/com/rt/springboot/app/controllers/LoginController.java
+++ src/main/java/com/rt/springboot/app/controllers/LoginController.java
@@ -12,7 +12,9 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class LoginController {
 
 	@GetMapping("/login")
-	public String login(@RequestParam(value = "error", required = false) String error,  Model model, Principal principal, RedirectAttributes flash) {
+	public String login(@RequestParam(value = "error", required = false) String error,  
+			@RequestParam(value = "logout", required = false) String logout, 
+			Model model, Principal principal, RedirectAttributes flash) {
 		
 		if(principal != null) {
 			flash.addAttribute("info", "Ya ha iniciado sesión anteriormente");
@@ -21,6 +23,10 @@ public class LoginController {
 		
 		if(error != null) {
 			model.addAttribute("error", "Error en el login: Nombre de usuario o contraseña incorrecta, por favor vuelva a intentarlo.");
+		}
+		
+		if(logout != null) {
+			model.addAttribute("success","Ha cerrado sesion con exito");
 		}
 		
 		return "login";

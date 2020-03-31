735327bc780959ea64ac3dd736cf958fb519e558
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-03-31 20:52:51 -0300

    agregado tabla productos en vista pdf

--- src/main/java/com/rt/springboot/app/view/pdf/InvoicePdfView.java
+++ src/main/java/com/rt/springboot/app/view/pdf/InvoicePdfView.java
@@ -9,9 +9,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.lowagie.text.Document;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfCell;
+import com.lowagie.text.pdf.PdfPCell;
 import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 import com.rt.springboot.app.models.entity.Invoice;
+import com.rt.springboot.app.models.entity.ItemInvoice;
 
 @Component("invoice/view")
 public class InvoicePdfView extends AbstractPdfView {
@@ -23,11 +27,13 @@ public class InvoicePdfView extends AbstractPdfView {
 		Invoice invoice = (Invoice) model.get("invoice");
 		
 		PdfPTable table = new PdfPTable(1);
+		table.setSpacingAfter(20);
 		table.addCell("Client Details");
 		table.addCell(invoice.getClient().getFirstName() + " " + invoice.getClient().getLastName());
 		table.addCell(invoice.getClient().getEmail());
 		
 		PdfPTable table2 = new PdfPTable(1);
+		table2.setSpacingAfter(20);
 		table2.addCell("Invoice Details: ");
 		table2.addCell("Page: " + invoice.getId());
 		table2.addCell("Description: " + invoice.getDescription());
@@ -35,6 +41,27 @@ public class InvoicePdfView extends AbstractPdfView {
 		
 		document.add(table);
 		document.add(table2);
+	
+		PdfPTable table3 = new PdfPTable(4);
+		table3.addCell("Product: ");
+		table3.addCell("Price: ");
+		table3.addCell("Amount: ");
+		table3.addCell("Total: ");
+		
+		for(ItemInvoice item : invoice.getItems()) {
+			table3.addCell(item.getProduct().getName());
+			table3.addCell(item.getProduct().getPrice().toString());
+			table3.addCell(item.getAmount().toString());
+			table3.addCell(item.calculateImport().toString());
+		}
+		
+		PdfPCell cell = new PdfPCell(new Phrase("Total: "));
+		cell.setColspan(3);
+		cell.setHorizontalAlignment(PdfCell.ALIGN_RIGHT);
+		table3.addCell(cell);
+		table3.addCell(invoice.getTotal().toString());
+		
+		document.add(table3);
 		
 	}
 	

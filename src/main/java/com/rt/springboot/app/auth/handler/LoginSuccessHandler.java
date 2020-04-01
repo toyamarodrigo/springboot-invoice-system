1a33b8dd0c65c16c9418a2f15628945bd311822a
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-03-31 22:02:55 -0300

    agregado archivo texto traduccion

--- src/main/java/com/rt/springboot/app/auth/handler/LoginSuccessHandler.java
+++ src/main/java/com/rt/springboot/app/auth/handler/LoginSuccessHandler.java
@@ -1,33 +1,46 @@
 package com.rt.springboot.app.auth.handler;
 
 import java.io.IOException;
+import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.support.SessionFlashMapManager;
 
 @Component
 public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+	
+	@Autowired
+	private MessageSource messageSource;
+	
+	@Autowired
+	private LocaleResolver localeResolver;
+	
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 			Authentication authentication) throws IOException, ServletException {
 		
 		SessionFlashMapManager flashMapManager = new SessionFlashMapManager();
-		
 		FlashMap flashMap = new FlashMap();
 		
-		flashMap.put("success", "Hola " + authentication.getName() + ", Has iniciado sesion con exito");
+		Locale locale = localeResolver.resolveLocale(request);
+		String message = String.format(messageSource.getMessage("text.login.success", null, locale), authentication.getName());
+		
+		flashMap.put("success", message);
 		flashMapManager.saveOutputFlashMap(flashMap, request, response);
 		
 		if(authentication != null) {
-			logger.info("El usuario '" + authentication.getName() + "' ha iniciado sesion con exito");
+			logger.info(message);
 		}
 		
 		super.onAuthenticationSuccess(request, response, authentication);
--- src/main/java/com/rt/springboot/app/view/pdf/InvoicePdfView.java
+++ src/main/java/com/rt/springboot/app/view/pdf/InvoicePdfView.java
@@ -1,11 +1,20 @@
 package com.rt.springboot.app.view.pdf;
 
+
+import java.awt.Color;
+import java.util.Locale;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.support.SessionFlashMapManager;
 import org.springframework.web.servlet.view.document.AbstractPdfView;
 
 import com.lowagie.text.Document;
@@ -23,46 +32,59 @@ public class InvoicePdfView extends AbstractPdfView {
 	@Override
 	protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
 			HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+
 		Invoice invoice = (Invoice) model.get("invoice");
 		
+		MessageSourceAccessor message = getMessageSourceAccessor();
+
+		// Detalles del Cliente
 		PdfPTable table = new PdfPTable(1);
 		table.setSpacingAfter(20);
-		table.addCell("Client Details");
+		PdfPCell cell = null;		
+		cell = new PdfPCell(new Phrase(message.getMessage("text.factura.ver.datos.cliente")));
+		cell.setBackgroundColor(new Color(184, 218, 255));
+		cell.setPadding(8f);
+		table.addCell(cell);
 		table.addCell(invoice.getClient().getFirstName() + " " + invoice.getClient().getLastName());
 		table.addCell(invoice.getClient().getEmail());
 		
+		// Detalles de Factura
 		PdfPTable table2 = new PdfPTable(1);
 		table2.setSpacingAfter(20);
-		table2.addCell("Invoice Details: ");
-		table2.addCell("Page: " + invoice.getId());
-		table2.addCell("Description: " + invoice.getDescription());
-		table2.addCell("Date: " + invoice.getCreateAt());
-		
-		document.add(table);
-		document.add(table2);
+		cell = new PdfPCell(new Phrase(message.getMessage("text.factura.ver.datos.cliente")));
+		cell.setBackgroundColor(new Color(195, 230, 203));
+		cell.setPadding(8f);
+		table2.addCell(cell);
+		table2.addCell(message.getMessage("text.cliente.factura.folio")+ ": " + invoice.getId());
+		table2.addCell(message.getMessage("text.cliente.factura.descripcion")+ ": " + invoice.getDescription());
+		table2.addCell(message.getMessage("text.cliente.factura.fecha")+ ": " + invoice.getCreateAt());
 	
+		// Tabla de Productos
 		PdfPTable table3 = new PdfPTable(4);
-		table3.addCell("Product: ");
-		table3.addCell("Price: ");
-		table3.addCell("Amount: ");
-		table3.addCell("Total: ");
+		table3.setWidths(new float[] {3.5f, 1, 1, 1});
+		table3.addCell(message.getMessage("text.factura.form.item.nombre"));
+		table3.addCell(message.getMessage("text.factura.form.item.precio"));
+		table3.addCell(message.getMessage("text.factura.form.item.cantidad"));
+		table3.addCell(message.getMessage("text.factura.form.item.total"));
 		
 		for(ItemInvoice item : invoice.getItems()) {
 			table3.addCell(item.getProduct().getName());
 			table3.addCell(item.getProduct().getPrice().toString());
-			table3.addCell(item.getAmount().toString());
+			cell = new PdfPCell(new Phrase(item.getAmount().toString()));
+			cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+			table3.addCell(cell);
 			table3.addCell(item.calculateImport().toString());
 		}
 		
-		PdfPCell cell = new PdfPCell(new Phrase("Total: "));
+		cell = new PdfPCell(new Phrase(message.getMessage("text.factura.form.total")));
 		cell.setColspan(3);
 		cell.setHorizontalAlignment(PdfCell.ALIGN_RIGHT);
 		table3.addCell(cell);
 		table3.addCell(invoice.getTotal().toString());
 		
+		document.add(table);
+		document.add(table2);
 		document.add(table3);
-		
 	}
 	
 	

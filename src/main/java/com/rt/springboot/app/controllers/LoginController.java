a2e4005380b1816bcef9cdb68bd203ed8145a3a3
toyamarodrigo <toyama.rodrigo@gmail.com>
2020-04-01 01:50:26 -0300

    added messages_es/en.properties to every view and controller

--- src/main/java/com/rt/springboot/app/controllers/LoginController.java
+++ src/main/java/com/rt/springboot/app/controllers/LoginController.java
@@ -1,7 +1,10 @@
 package com.rt.springboot.app.controllers;
 
 import java.security.Principal;
+import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,22 +14,25 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @Controller
 public class LoginController {
 
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping("/login")
 	public String login(@RequestParam(value = "error", required = false) String error,  
 			@RequestParam(value = "logout", required = false) String logout, 
-			Model model, Principal principal, RedirectAttributes flash) {
+			Model model, Principal principal, RedirectAttributes flash, Locale locale) {
 		
 		if(principal != null) {
-			flash.addAttribute("info", "Ya ha iniciado sesión anteriormente");
+			flash.addAttribute("info", messageSource.getMessage("text.login.already", null, locale));
 			return "redirect:/";
 		}
 		
 		if(error != null) {
-			model.addAttribute("error", "Error en el login: Nombre de usuario o contraseña incorrecta, por favor vuelva a intentarlo.");
+			model.addAttribute("error", messageSource.getMessage("text.login.error", null, locale));
 		}
 		
 		if(logout != null) {
-			model.addAttribute("success","Ha cerrado sesion con exito");
+			model.addAttribute("success", messageSource.getMessage("text.login.logout", null, locale));
 		}
 		
 		return "login";
